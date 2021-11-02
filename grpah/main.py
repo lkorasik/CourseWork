dfe94a6524c2a13317bb9dbc773f6f43e2b077fa
Lev <lkorasik83@gmail.com>
2021-11-02 09:39:03 +0500

    Добавил пару формул

--- grpah/main.py
+++ grpah/main.py
@@ -4,8 +4,8 @@ from parameters import Parameters
 if __name__ == "__main__":
     params = Parameters()
 
-    #Builder.bifurcation_wrap(params, True)
-    Builder.bifurcation_and_down_stable_wrap(params, False)
+    Builder.bifurcation_wrap(params, False)
+    #Builder.bifurcation_and_down_stable_wrap(params, False)
     #Builder.time_series_wrap(params, True)
     #Builder.single_newton_wrap(params)
     #Builder.lamerei_wrap(params, False)
