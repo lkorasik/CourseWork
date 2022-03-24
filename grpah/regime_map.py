8daf22d0766db2c7eb83bf8e33082f8f7d19b39c
lev <lkorasik83@gmail.com>
2022-03-24 08:58:25 +0500

    ref

--- grpah/regime_map.py
+++ grpah/regime_map.py
@@ -1,31 +1,25 @@
 from collections import Counter
 
-from matplotlib import pyplot as plt
 
-
-def regime_map(x_start, a_range, b_range, time_range, f):
+def regime_map(x_start, a_range, b_range, time_range, f, file_path):
     result = dict()
-    newtoon = dict()
     for a in a_range:
         fk = round(a, 3)
         result[fk] = dict()
-        newtoon[fk] = dict()
         print(fk)
         for b in b_range:
             sc = round(b, 3)
             result[fk][sc] = []
             x0 = x_start
-            for t in time_range:
+            for _ in time_range:
                 xt = f(a, b, x0)
                 x0 = xt
-            # for t in time_range:
             for t in range(20):
                 xt = f(a, b, x0)
                 result[fk][sc].append(xt)
                 x0 = xt
 
     print("data collected")
-    fig, ax = plt.subplots()
 
     res = dict()
     for j in range(1, 10 + 1):
@@ -38,8 +32,10 @@ def regime_map(x_start, a_range, b_range, time_range, f):
                 for i in range(len(data)):
                     data[i] = round(data[i], 3)
 
+                # используй set
                 di = Counter(data)
                 # print(fk, sc, Counter(data))
+                # можно забить на это условие?
                 if len(di.keys()) == j:
                     res[j].append([fk, sc, di.keys()])
                     continue
@@ -48,22 +44,22 @@ def regime_map(x_start, a_range, b_range, time_range, f):
         for i in res[j]:
             print(i[0], i[1], set(i[2]))
 
-    peq = open("C:\\Users\\lkora\\Desktop\\data\\eqX2Gt2X1.txt", 'w')
-    peq1 = open("C:\\Users\\lkora\\Desktop\\data\\eqX2Lt2X1.txt", 'w')
-    c2 = open('C:\\Users\\lkora\\Desktop\\data\\cycle2.txt', 'w')
-    c3 = open('C:\\Users\\lkora\\Desktop\\data\\cycle3.txt', 'w')
-    c4 = open('C:\\Users\\lkora\\Desktop\\data\\cycle4.txt', 'w')
-    c5 = open('C:\\Users\\lkora\\Desktop\\data\\cycle5.txt', 'w')
-    c6 = open('C:\\Users\\lkora\\Desktop\\data\\cycle6.txt', 'w')
-    c7 = open('C:\\Users\\lkora\\Desktop\\data\\cycle7.txt', 'w')
-    c8 = open('C:\\Users\\lkora\\Desktop\\data\\cycle8.txt', 'w')
-    c9 = open('C:\\Users\\lkora\\Desktop\\data\\cycle9.txt', 'w')
-    c11 = open('C:\\Users\\lkora\\Desktop\\data\\cycle11.txt', 'w')
-    c10 = open('C:\\Users\\lkora\\Desktop\\data\\cycle10.txt', 'w')
-    c12 = open('C:\\Users\\lkora\\Desktop\\data\\cycle12.txt', 'w')
-    c13 = open('C:\\Users\\lkora\\Desktop\\data\\cycle13.txt', 'w')
-    c14 = open('C:\\Users\\lkora\\Desktop\\data\\cycle14.txt', 'w')
-    c15 = open('C:\\Users\\lkora\\Desktop\\data\\cycle15.txt', 'w')
+    peq = open(file_path + "eqX2Gt2X1.txt", "w")
+    peq1 = open(file_path + "eqX2Lt2X1.txt", 'w')
+    c2 = open(file_path + 'cycle2.txt', 'w')
+    c3 = open(file_path + 'cycle3.txt', 'w')
+    c4 = open(file_path + 'cycle4.txt', 'w')
+    c5 = open(file_path + 'cycle5.txt', 'w')
+    c6 = open(file_path + 'cycle6.txt', 'w')
+    c7 = open(file_path + 'cycle7.txt', 'w')
+    c8 = open(file_path + 'cycle8.txt', 'w')
+    c9 = open(file_path + 'cycle9.txt', 'w')
+    c11 = open(file_path + 'cycle11.txt', 'w')
+    c10 = open(file_path + 'cycle10.txt', 'w')
+    c12 = open(file_path + 'cycle12.txt', 'w')
+    c13 = open(file_path + 'cycle13.txt', 'w')
+    c14 = open(file_path + 'cycle14.txt', 'w')
+    c15 = open(file_path + 'cycle15.txt', 'w')
 
     for j in range(1, 10 + 1):
         line = ""
@@ -112,6 +108,7 @@ def regime_map(x_start, a_range, b_range, time_range, f):
         if j == 15:
             c15.write(line)
 
+    peq1.close()
     peq.close()
     c2.close()
     c3.close()
@@ -127,47 +124,3 @@ def regime_map(x_start, a_range, b_range, time_range, f):
     c13.close()
     c14.close()
     c15.close()
-
-    # print("Attractors found")
-    # colors = {1: 'red',
-    #           2: 'steelblue',
-    #           3: 'green',
-    #           4: 'lime',
-    #           5: 'darkviolet',
-    #           6: 'deeppink',
-    #           7: 'aqua',
-    #           8: 'navy',
-    #           9: 'pink',
-    #           10: 'brown',
-    #           11: 'gold',
-    #           12: 'dodgerblue'}
-    #
-    # colors = {1: [204 / 255, 204 / 255, 204 / 255],
-    #           2: [76 / 255, 102 / 255, 0 / 255],
-    #           3: [255 / 255, 0 / 255, 0 / 255],
-    #           4: [168 / 255, 2 / 255, 168 / 255],
-    #           5: [0 / 255, 253 / 255, 255 / 255],
-    #           6: [254 / 255, 255 / 255, 0 / 255],
-    #           7: [120 / 255, 55 / 255, 219 / 255],
-    #           8: [114 / 255, 153 / 255, 0 / 255],
-    #           9: [134 / 255, 18 / 255, 252 / 255],
-    #           10: [250 / 255, 145 / 255, 145 / 255],
-    #           11: [128 / 255, 116 / 255, 98 / 255],
-    #           12: [0 / 255, 255 / 255, 0 / 255],
-    #           13: [127 / 255, 0 / 255, 128 / 255],
-    #           14: [126 / 255, 127 / 255, 247 / 255],
-    #           15: [0 / 255, 1 / 255, 255 / 255]}
-    #
-    # for i in res.keys():
-    #     for item in res[i]:
-    #         plt.scatter(item[0], item[1], color=colors[i], linewidths=0.01)
-    #
-    # print("Data plotted")
-    # patches = [mpatches.Patch(color=colors[i], label=i) for i in colors.keys()]
-    # ax.legend(handles=patches, loc="center left", bbox_to_anchor=(1, 0.5))
-    # plt.ylim(0.01, 0.6)
-    # plt.xlim(0.01, 2)
-    #
-    # plt.savefig("C:\\users\\lkora\\desktop\\a.jpg")
-
-    # plt.show()
