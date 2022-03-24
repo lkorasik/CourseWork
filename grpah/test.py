03f5fc8059fe08576b953c2d853a0f09f51db40b
lev <lkorasik83@gmail.com>
2022-03-24 12:40:50 +0500

    Ref

--- grpah/test.py
+++ grpah/test.py
@@ -2,8 +2,6 @@ from julia import Main
 
 
 if __name__ == "__main__":
-    print("HW")
-    print(Main.eval('A = rand(3, 3)'))
-    Main.eval('include(\"test.jl\")')
-    print(Main.eval('Test.f(3)'))
+    Main.include("test.jl")
     print(Main.Test.f(3))
+
