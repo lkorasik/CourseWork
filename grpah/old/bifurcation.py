437892b2361914665d4cc8491c8d38dfeece8a8a
lev <lkorasik83@gmail.com>
2022-04-05 19:15:05 +0500

    Update bifurcation.py

--- grpah/old/bifurcation.py
+++ grpah/old/bifurcation.py
@@ -18,8 +18,8 @@ def bifurcation(time_range, x_start, p_range, f, up_border=10_000, down_border=1
 
     for p in p_range:
         values[p] = []
+        
         x_0 = x_start
-
         for _ in time_range:
             x_t = f(p, x_0)
             if up_border is not None and abs(x_t) > up_border:
