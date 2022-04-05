0904f54a93fe6c05d4a659f608c71b1599cb9946
lev <lkorasik83@gmail.com>
2022-04-05 18:03:51 +0500

    Update base_functions.py

--- grpah/functions_pkg/base_functions.py
+++ grpah/functions_pkg/base_functions.py
@@ -1,11 +1,9 @@
 from sympy import lambdify
-from functions_pkg.symbols import a, b, x, eta
+from functions_pkg.symbols import a, b, x
 
 _f = (a * x ** 2) / ((b + x) ** 6)
+f = lambdify([a, b, x], _f)
 
 
 def f_subs(a_, b_, x_):
     return float(_f.subs(a, a_).subs(b, b_).subs(x, x_))
-
-
-f = lambdify([a, b, x], _f)
