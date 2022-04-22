1b7ccd101b69351cda58f865d8f6918973cfc288
lev <lkorasik83@gmail.com>
2022-04-22 19:33:06 +0500

    Update cyclical_mean.py

--- grpah/old/cyclical_mean.py
+++ grpah/old/cyclical_mean.py
@@ -11,8 +11,7 @@ def cyclical_mean(time_range, x_start, b_range, f, count):
 
     data = []
     for i in range(count):
-        values = bifurcation(time_range, x_start, b_range, f,
-                             down_border=None)
+        values = bifurcation(time_range, x_start, b_range, f, down_border=None)
         draw_y = mean(b_range, values)[1]
         data.append(draw_y)
 
