09b34527c376a55c688297f5d2e9f2d0a88d8d92
lev <lkorasik83@gmail.com>
2022-04-25 17:12:46 +0500

    Update bifurcation_with_ssf.py

--- grpah/old/bifurcation_with_ssf.py
+++ grpah/old/bifurcation_with_ssf.py
@@ -43,9 +43,9 @@ def bifurcation_with_ssf(b_range, a, borders, m, epsilon, values, f, s, q, s_, q
         x0, x1 = split(x, 2)
 
         for i in range(len(x0)):
-            r = Alg.get_m(2)
-            m1_ = float(r[0].subs(symbols.a, a).subs(symbols.b, b).subs(Symbol("x1"), x0[i]).subs(Symbol("x2"), x1[i]))
-            m2_ = float(r[1].subs(symbols.a, a).subs(symbols.b, b).subs(Symbol("x1"), x0[i]).subs(Symbol("x2"), x1[i]))
+            all_m = Alg.get_m(2)
+            m1_ = float(all_m[0].subs(symbols.a, a).subs(symbols.b, b).subs(Symbol("x1"), x0[i]).subs(Symbol("x2"), x1[i]))
+            m2_ = float(all_m[1].subs(symbols.a, a).subs(symbols.b, b).subs(Symbol("x1"), x0[i]).subs(Symbol("x2"), x1[i]))
 
             for line in lines:
                 line.add_x(b)
@@ -68,11 +68,11 @@ def bifurcation_with_ssf(b_range, a, borders, m, epsilon, values, f, s, q, s_, q
         x0, x1, x2, x3 = split(x, 4)
 
         for i in range(len(x0)):
-            r = Alg.get_m(4)
-            m1_ = float(r[0].subs(symbols.a, a).subs(symbols.b, b).subs(Symbol("x1"), x0[i]).subs(Symbol("x2"), x1[i]).subs(Symbol("x3"), x2[i]).subs(Symbol("x4"), x3[i]))
-            m2_ = float(r[1].subs(symbols.a, a).subs(symbols.b, b).subs(Symbol("x1"), x0[i]).subs(Symbol("x2"), x1[i]).subs(Symbol("x3"), x2[i]).subs(Symbol("x4"), x3[i]))
-            m3_ = float(r[2].subs(symbols.a, a).subs(symbols.b, b).subs(Symbol("x1"), x0[i]).subs(Symbol("x2"), x1[i]).subs(Symbol("x3"), x2[i]).subs(Symbol("x4"), x3[i]))
-            m4_ = float(r[3].subs(symbols.a, a).subs(symbols.b, b).subs(Symbol("x1"), x0[i]).subs(Symbol("x2"), x1[i]).subs(Symbol("x3"), x2[i]).subs(Symbol("x4"), x3[i]))
+            all_m = Alg.get_m(4)
+            m1_ = float(all_m[0].subs(symbols.a, a).subs(symbols.b, b).subs(Symbol("x1"), x0[i]).subs(Symbol("x2"), x1[i]).subs(Symbol("x3"), x2[i]).subs(Symbol("x4"), x3[i]))
+            m2_ = float(all_m[1].subs(symbols.a, a).subs(symbols.b, b).subs(Symbol("x1"), x0[i]).subs(Symbol("x2"), x1[i]).subs(Symbol("x3"), x2[i]).subs(Symbol("x4"), x3[i]))
+            m3_ = float(all_m[2].subs(symbols.a, a).subs(symbols.b, b).subs(Symbol("x1"), x0[i]).subs(Symbol("x2"), x1[i]).subs(Symbol("x3"), x2[i]).subs(Symbol("x4"), x3[i]))
+            m4_ = float(all_m[3].subs(symbols.a, a).subs(symbols.b, b).subs(Symbol("x1"), x0[i]).subs(Symbol("x2"), x1[i]).subs(Symbol("x3"), x2[i]).subs(Symbol("x4"), x3[i]))
 
             for line in lines:
                 line.add_x(b)
