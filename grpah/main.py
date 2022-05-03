08a4c86e6b58d5e78d8b08569d089784d13ced65
lev <lkorasik83@gmail.com>
2022-05-03 22:39:16 +0500

    Update main.py

--- grpah/main.py
+++ grpah/main.py
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     # run_stochastic_sensitivity_a_noise_to_file()
     # run_stochastic_sensitivity_additive_noise_to_file()
 
-    run_machalanobis_beta_noise()
+    # run_machalanobis_beta_noise()
     # run_machalanobis_alpha_noise()
     # run_machalanobis_additive_noise()
 
