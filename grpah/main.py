49e0ad89f0b1497ec0c4413d21254271511b8b85
lev <lkorasik83@gmail.com>
2022-05-03 22:21:48 +0500

    Upd

--- grpah/main.py
+++ grpah/main.py
@@ -77,13 +77,13 @@ if __name__ == "__main__":
     # run_m_b_additive_noise()
 
     # Выгрузить в файл данные по функции стохастической чувствительности
-    run_stochastic_sensitivity_b_noise_to_file()
-    run_stochastic_sensitivity_a_noise_to_file()
-    run_stochastic_sensitivity_additive_noise_to_file()
+    # run_stochastic_sensitivity_b_noise_to_file()
+    # run_stochastic_sensitivity_a_noise_to_file()
+    # run_stochastic_sensitivity_additive_noise_to_file()
 
-    # erunda_beta_noise()
-    # erunda_alpha_noise()
-    # ernuda_additive_noise()
+    # run_machalanobis_beta_noise()
+    # run_machalanobis_alpha_noise()
+    run_machalanobis_additive_noise()   
 
     # critical_intensity_beta_noise()
     # critical_intensity_alpha_noise()
