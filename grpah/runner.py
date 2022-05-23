381f1a8064945b0cf0bdd720470d3eabd6bcd768
lev <lkorasik83@gmail.com>
2022-05-23 19:53:44 +0500

    Upd

--- grpah/runner.py
+++ grpah/runner.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import patches, pyplot as plt
 
 import functions
 from algorithms.absorbing_area import absorbing_area
@@ -14,6 +15,7 @@ from algorithms.lamerei import lamerei
 from algorithms.lyapunov import lyapunov
 from algorithms.m_b import m_b
 from algorithms.mean import mean
+from algorithms.regime_map import regime_map
 from algorithms.time_series import time_series
 from algorithms.variance import variance
 from functions_pkg import functions_b_noise, function, functions_a_noise, functions_additive_noise, others
@@ -615,6 +617,17 @@ def run_equilibrium():
     plotter.show_last()
 
 
+def run_regime_map():
+    regime_map(
+        x_start=0.2,
+        a_range=np.arange(0.01, 2, 0.001),
+        b_range=np.arange(0.01, 0.6, 0.001),
+        time_range=range(1, 10000 + 1),
+        f=function.f,
+        file_path="C:\\Users\\lkora\\Desktop\\data\\"
+    )
+
+
 def run_mean():
     values = bifurcation(
         time_range=range(1, 1000 + 1),
@@ -830,7 +843,7 @@ def run_cyclic_variance():
 def run_stochastic_sensitivity_b_noise():
     p_range = np.arange(0.22, 0.582355932, 0.001)
 
-    epsilon = 0.01
+    epsilon = 0.001
 
     values = bifurcation(
         time_range=range(1, 100 + 1),
@@ -860,29 +873,41 @@ def run_stochastic_sensitivity_b_noise():
     )
     chaos = convert_dict_to_lists(chaos)
 
+    # .adjust(top=0.92, bottom=0.15, left=0.175, right=0.95)
+    # .setup_x_label('t', font_size=25, label_pad=0)
+    # .setup_x_ticks(font_size=20)
+    # .setup_y_label('x', font_size=25, label_pad=12)
+    # .setup_y_ticks(font_size=20)
+
+    # Create a Rectangle patch
+    # rect = patches.Rectangle((50, 100), 40, 30, linewidth=1, edgecolor='r', facecolor='none')
+
     plotter = (Plotter()
-               ._setup('$\\beta$', 'x', 'log', 'major', 'Bifurcation with $\\beta$-noise')
-               .scatter(chaos[0], chaos[1], '.', colors.steel_blue))
+                # ._setup('$\\beta$', 'x', 'log', 'major', 'Bifurcation with $\\beta$-noise')
+                .setup_x_label('$\\beta$', font_size=13, label_pad=2)
+                .setup_y_label('x', font_size=13, label_pad=6)
+                .setup_x_ticks(font_size=13)
+                .setup_y_ticks(font_size=13)
+                .setup_y_scale(scale.log)
+                .setup_grid(grid.major)
+                .setup_title('Bifurcation with $\\beta$-noise')
+                .scatter(chaos[0], chaos[1], '.', colors.steel_blue))
 
     for line in source:
         plotter.plot(line.x, line.y, ',', colors.red)
 
-    # plotter.plot_line(source[0], ',', 'orange')
-    # plotter.plot_line(source[3], ',', 'orange')
-    # plotter.plot_line(source[7], ',', 'orange')
-    # plotter.plot_line(source[14], ',', 'orange')
-
-    # plotter.plot_line(source[1], ',', 'navy')
-    # plotter.plot_line(source[4], ',', 'navy')
-    # plotter.plot_line(source[10], ',', 'navy')
-    # plotter.plot_line(source[15], ',', 'navy')
+    (plotter
+        .add_rectangle(0.451, 0.275, 0.025, 0.3)
+        .add_rectangle(0.352, 0.7, 0.035, 0.9)
+        .add_rectangle(0.24, 0.0003, 0.03, 0.0017)
+        .add_rectangle(0.252, 2, 0.03, 7.4))
 
     plotter.show_last()
     # plotter.show()
 
 
 def run_stochastic_sensitivity_b_noise_1():
-    p_range = np.arange(0.35, 0.385, 0.0001)
+    p_range = np.arange(0.451, 0.476, 0.0001)
 
     values = bifurcation(
         time_range=range(1, 100 + 1),
@@ -913,8 +938,16 @@ def run_stochastic_sensitivity_b_noise_1():
     chaos = convert_dict_to_lists(chaos)
 
     plotter = (Plotter()
-               ._setup('$\\beta$', 'x', 'log', 'major', 'Bifurcation with $\\beta$-noise')
-               .scatter(chaos[0], chaos[1], '.', 'steelblue'))
+                .setup_x_limit(0.451, 0.476)
+                .setup_y_limit(0.275, 0.575)
+                # ._setup('$\\beta$', 'x', 'log', 'major', 'Bifurcation with $\\beta$-noise')
+                .adjust(top=0.92, bottom=0.15, left=0.175, right=0.95)
+                .setup_x_label('$\\beta$', font_size=25, label_pad=0)
+                .setup_x_ticks(font_size=20)
+                .setup_y_label('x', font_size=25, label_pad=12)
+                .setup_y_ticks(font_size=20)
+                .setup_grid(grid.major)
+                .scatter(chaos[0], chaos[1], '.', 'steelblue'))
 
     for line in source:
         plotter.plot(line.x, line.y, ',', 'red')
@@ -924,7 +957,7 @@ def run_stochastic_sensitivity_b_noise_1():
 
 
 def run_stochastic_sensitivity_b_noise_2():
-    p_range = np.arange(0.4, 0.6, 0.0001)
+    p_range = np.arange(0.352, 0.387, 0.0001)
 
     values = bifurcation(
         time_range=range(1, 100 + 1),
@@ -955,7 +988,14 @@ def run_stochastic_sensitivity_b_noise_2():
     chaos = convert_dict_to_lists(chaos)
 
     plotter = (Plotter()
-               ._setup('$\\beta$', 'x', 'log', 'major', 'Bifurcation with $\\beta$-noise')
+                .setup_x_limit(0.352, 0.387)
+                .setup_y_limit(0.7, 1.6)
+                .adjust(top=0.92, bottom=0.15, left=0.175, right=0.95)
+                .setup_x_label('$\\beta$', font_size=25, label_pad=0)
+                .setup_x_ticks(ticks=np.arange(0.352, 0.387, 0.01), font_size=20)
+                .setup_y_label('x', font_size=25, label_pad=12)
+                .setup_y_ticks(font_size=20)
+               # ._setup('$\\beta$', 'x', 'log', 'major', 'Bifurcation with $\\beta$-noise')
                .scatter(chaos[0], chaos[1], '.', 'steelblue'))
 
     for line in source:
@@ -966,7 +1006,7 @@ def run_stochastic_sensitivity_b_noise_2():
 
 
 def run_stochastic_sensitivity_b_noise_3():
-    p_range = np.arange(0.22, 0.3, 0.0001)
+    p_range = np.arange(0.24, 0.27, 0.0001)
 
     values = bifurcation(
         time_range=range(1, 100 + 1),
@@ -997,6 +1037,52 @@ def run_stochastic_sensitivity_b_noise_3():
     chaos = convert_dict_to_lists(chaos)
 
     plotter = (Plotter()
+                .setup_x_limit(0.24, 0.27)
+                .setup_y_limit(0.0003, 0.002)
+               ._setup('$\\beta$', 'x', 'log', 'major', 'Bifurcation with $\\beta$-noise')
+               .scatter(chaos[0], chaos[1], '.', 'steelblue'))
+
+    for line in source:
+        plotter.plot(line.x, line.y, ',', 'red')
+
+    plotter.show_last()
+    # plotter.show()
+
+
+def run_stochastic_sensitivity_b_noise_4():
+    p_range = np.arange(0.252, 0.282, 0.0001)
+
+    values = bifurcation(
+        time_range=range(1, 100 + 1),
+        x_start=0.2,
+        p_range=p_range,
+        f=lambda b, x: function.f(1, b, x),
+    )
+    source = bifurcation_with_ssf(
+        values=values,
+        b_range=p_range,
+        a=1,
+        borders=[[0.44, 0.582355932], [0.379, 0.435], [0.36, 0.37], [0.22, 0.34]],
+        m=lambda a, b, x: functions_b_noise.m(a, b, x, 0.001),
+        epsilon=0.001,
+        f=lambda b, x: function.f(1, b, x),
+        s=lambda b, x: functions_b_noise.s(1, b, x, 0.001),
+        q=lambda b, x: functions_b_noise.q(1, b, x, 0.001),
+        q_=functions_b_noise._q,
+        s_=functions_b_noise._s
+    )
+    chaos = bifurcation(
+        time_range=range(1, 100 + 1),
+        x_start=0.2,
+        p_range=p_range,
+        f=lambda b, x: functions_b_noise.f(1, b, x, 0.001)
+        # f=lambda b, x: functions.f_pb(1, b, x, 0.001)
+    )
+    chaos = convert_dict_to_lists(chaos)
+
+    plotter = (Plotter()
+                .setup_x_limit(0.252, 0.282)
+                .setup_y_limit(2, 9.4)
                ._setup('$\\beta$', 'x', 'log', 'major', 'Bifurcation with $\\beta$-noise')
                .scatter(chaos[0], chaos[1], '.', 'steelblue'))
 
