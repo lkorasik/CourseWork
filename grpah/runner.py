787c8886446b9e20158b7a3f24994a7ae0618aed
lev <lkorasik83@gmail.com>
2022-06-04 14:38:57 +0500

    Upd

--- grpah/runner.py
+++ grpah/runner.py
@@ -810,8 +810,11 @@ def run_cyclic_mean():
     )
 
     (Plotter()
-        .setup_x_label('$\\beta$')
-        .setup_y_label('x')
+        .adjust(left=0.125, right=0.9, top=0.92, bottom=0.15)
+        .setup_x_label('$\\beta$', font_size=20)
+        .setup_y_label('x', font_size=20, label_pad=10)
+        .setup_y_ticks(font_size=20)
+        .setup_x_ticks(font_size=20, ticks=np.arange(0.2, 0.6, 0.1))
         .setup_y_scale(scale.linear)
         .setup_grid(grid.major)
         # .setup_title('EV cyclic')
@@ -819,7 +822,7 @@ def run_cyclic_mean():
         .plot_line(source1, markers.point, colors.red, '$\\varepsilon = 0.01$')
         .plot_line(source2, markers.point, colors.green, '$\\varepsilon = 0.03$')
         .plot_line(source3, markers.point, colors.black, '$\\varepsilon = 0.04$')
-        .legend()
+        .legend(font_size=15)
         .show_last())
 
 
@@ -890,7 +893,7 @@ def run_cyclic_variance():
         x_start=0.2,
         b_range=np.arange(0.22, 0.582355932, 0.01),
         f=lambda b, x: function.f(1, b, x),
-        count=100
+        count=1000
     )
     source1 = cyclical_variance(
         time_range=range(1, 100 + 1),
@@ -898,7 +901,7 @@ def run_cyclic_variance():
         b_range=np.arange(0.22, 0.582355932, 0.01),
         f=lambda b, x: functions_b_noise.f(1, b, x, 0.01),
         # f=lambda b, x: functions.f_pb(1, b, x, 0.01),
-        count=100
+        count=1000
     )
     source2 = cyclical_variance(
         time_range=range(1, 100 + 1),
@@ -906,7 +909,7 @@ def run_cyclic_variance():
         b_range=np.arange(0.22, 0.582355932, 0.01),
         f=lambda b, x: functions_b_noise.f(1, b, x, 0.03),
         # f=lambda b, x: functions.f_pb(1, b, x, 0.03),
-        count=100
+        count=1000
     )
     source3 = cyclical_variance(
         time_range=range(1, 100 + 1),
@@ -914,13 +917,13 @@ def run_cyclic_variance():
         b_range=np.arange(0.22, 0.582355932, 0.01),
         f=lambda b, x: functions_b_noise.f(1, b, x, 0.04),
         # f=lambda b, x: functions.f_pb(1, b, x, 0.04),
-        count=100
+        count=1000
     )
 
     (Plotter()
         .setup_x_label('$\\beta$')
         .setup_y_label('x')
-        .setup_y_scale(scale.linear)
+        .setup_y_scale(scale.log)
         .setup_grid(grid.major)
         # .setup_title('Variance cyclic')
         .plot_line(source0, '.', colors.steel_blue, '$\\varepsilon = 0$')
