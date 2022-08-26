40b9feb1fdcbde172afba26c39c7988e34e0f85b
lev <lkorasik83@gmail.com>
2022-08-26 22:43:36 +0500

    Add command for interupt workers

--- grpah/mp.py
+++ grpah/mp.py
@@ -3,16 +3,45 @@ from time import sleep
 
 
 class Task:
-    def __init__(self, uid, func):
+    def __init__(self, uid, func, args, kwargs):
         self._uid = uid
         self._func = func
+        self._args = args
+        self._kwargs = kwargs
 
     def perform(self):
-        self._func()
+        self._func(*self._args, **self._kwargs)
 
 
-def worker(id_, tasks: Queue):
-    while tasks.qsize() != 0:
+class Dispatcher:
+    def __init__(self):
+        self._tasks = Queue()
+        self._commands = Queue()
+        self._worker_count = 2
+        self._workers = [Process(target=executor2, args=(i, self._tasks, self._commands)) for i in range(self._worker_count)]
+
+    def add_task(self, task):
+        self._tasks.put(task)
+
+    def start(self):
+        for worker in self._workers:
+            worker.start()
+
+    def stop(self):
+        for _ in range(len(self._workers)):
+            self._commands.put("stop")
+
+    def wait(self):
+        for worker in self._workers:
+            worker.join()
+
+
+def executor2(id_, tasks: Queue, commands: Queue):
+    while True:
+        command = commands.get()
+        if command == "stop":
+            break
+
         task = tasks.get()
         task.perform()
         print("Task " + str(task._uid) + " done by worker " + str(id_))
@@ -23,23 +52,20 @@ def f():
 
 
 if __name__ == "__main__":
-    tasks = Queue()
+    dispatcher = Dispatcher()
 
     for i in range(10):
-        task = Task(i, f)
-        tasks.put(task)
+        task = Task(i, f, [], {})
+        dispatcher.add_task(task)
 
     print("Tasks created")
 
-    worker0 = Process(target=worker, args=(0, tasks))
-    worker1 = Process(target=worker, args=(1, tasks))
-
-    worker0.start()
-    worker1.start()
+    dispatcher.start()
 
     print("Thread started")
 
-    worker0.join()
-    worker1.join()
+    dispatcher.stop()
+
+    # dispatcher.wait()
 
     print("All work done")
