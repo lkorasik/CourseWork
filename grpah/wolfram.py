ae3fa19b0047f578b2b4d1b73c16ccd08c1bf052
lev <lkorasik83@gmail.com>
2022-09-12 13:27:12 +0500

    Example of usage Wolfram from python

--- grpah/wolfram.py
+++ grpah/wolfram.py
@@ -1,12 +1,29 @@
 from wolframclient.evaluation import WolframLanguageSession
-from wolframclient.language import wlexpr
+from wolframclient.language import wlexpr, wl
 
 if __name__ == "__main__":
-    session = WolframLanguageSession()
-    resuilt = session.evaluate(wlexpr('Range[5]'))
-    print(resuilt)
-    resuilt = session.evaluate(wlexpr('f[x_] = 2 * x'))
-    print(resuilt)
-    resuilt = session.evaluate(wlexpr('D[f[x], x]'))
-    print(resuilt)
-    session.stop()
+    with WolframLanguageSession() as session:
+
+        result = session.evaluate(wlexpr('Range[5]'))
+        print(result)
+
+        result = session.evaluate('Range[5]')
+        print(result)
+
+        result = session.evaluate(wl.Range(5))
+        print(result)
+
+        result = wl.BarChart([1, 2, 3])
+        expr = wl.Export("C:/users/lkora/Desktop/t.png", result, 'PNG')
+        session.evaluate(expr)
+
+        result = session.evaluate("f[x_] := 2 * x")
+        print(session.evaluate('f[2]'))
+
+        result = wlexpr('f[x_] := 3 * x')
+        session.evaluate(result)
+        result = session.function(wlexpr('f'))
+        print(result(3))
+
+        future = session.evaluate_future('Pause[3]; 1 + 1')
+        print(future.result())
