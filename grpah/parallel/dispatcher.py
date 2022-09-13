43092540696668dbbae46b89b3dd4d9ac0ded643
Lev Korasik <lkorasik83@gmail.com>
2022-09-13 17:45:53 +0500

    Fix dispatcher

--- grpah/parallel/dispatcher.py
+++ grpah/parallel/dispatcher.py
@@ -1,4 +1,4 @@
-from multiprocessing import Queue, cpu_count
+from multiprocessing import cpu_count, Manager
 
 from parallel.events.stop_event import StopEvent
 from parallel.events.task_finished_event import TaskFinishedEvent
@@ -12,16 +12,19 @@ class Dispatcher:
         self._task_finished_event = TaskFinishedEvent()
         self._stop_event = StopEvent()
 
-        self._tasks = Queue()
-        self._results = Queue()
+        self._manager = Manager()
+
+        self._tasks = self._manager.Queue()
+        self._results = self._manager.Queue()
         self._worker_count = workers_count
         self._workers = [self._create_worker(i) for i in range(self._worker_count)]
 
     def _create_worker(self, uid):
-        return Worker(uid, self._tasks, self._results, self._task_finished_event, self._stop_event)
+        return Worker(uid, self._tasks, self._results, self._task_finished_event, self._stop_event, self._manager)
 
     def add_task(self, task):
         """Добавить задачу в очередь"""
+        print("Task " + str(task.get_uid()) + " added to queue")
         self._tasks.put(task)
 
     def start(self):
