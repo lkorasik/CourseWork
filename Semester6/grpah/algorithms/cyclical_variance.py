4013582c756f284ee323898fc0992fafc560bae4
lkorasik <lkorasik83@gmail.com>
2023-08-19 12:24:44 +0500

    Удалил параллельность

--- Semester6/grpah/algorithms/cyclical_variance.py
+++ Semester6/grpah/algorithms/cyclical_variance.py
@@ -5,7 +5,6 @@ from core.algorithms.old.variance import variance
 from visual.line import Line
 
 
-# todo: уточни правильно ли реализован алгоритм...
 def cyclical_variance(time_range, x_start, b_range, f, count):
     line = Line()
     for b in b_range:
